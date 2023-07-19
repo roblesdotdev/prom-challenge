1aa0fd59ce5cb471d793ffd3226d84858f5bbf0e
roblesdotdev <httpsnip@gmail.com>
2023-07-19 12:42:40 -0300

    setup lint & prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Promarketing",
-  description: "Desafio técnico para promarketing",
-};
+  title: 'Promarketing',
+  description: 'Desafio técnico para promarketing',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="antialiased bg-slate-100 text-slate-800">
+    <html lang="en" className="bg-slate-100 text-slate-800 antialiased">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <div>
       <h1>Work in progress...</h1>
     </div>
-  );
+  )
 }
